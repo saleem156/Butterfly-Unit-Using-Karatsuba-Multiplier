561fc313a8c7c2727d7f73c4d4070cbc3b477d40
saleem <mahammadsaleem156@gmail.com>
2026-09-12 17:19:00 +0530

    Update KM.sv

--- KM.sv
+++ KM.sv
@@ -1,55 +1,70 @@
-module KM (
-    input  logic        clk,
-    input  logic        rst_n,
-    input  logic [11:0] A,
-    input  logic [11:0] B,
 
-    output logic [23:0] PRODUCT
-);
+module KM(
+    input logic clk,
+    input logic rst_n,
+    input logic [11:0]a,
+    input logic [11:0]b,
+    output logic [11:0]h, //Higher bits
+    output logic [11:0]l); //Lower bits
 
-    logic [5:0]  A0, A1;
-    logic [5:0]  B0, B1;
+logic [5:0] A0, A1, B0, B1;
 
-    logic [11:0] Z0;
-    logic [11:0] Z2;
+assign A0 = a[11:0];
+assign A1 = a[11:6];
 
-    logic [6:0]  SUM_A;
-    logic [6:0]  SUM_B;
+assign B0 = b[5:0];
+assign B1 = b[11:6];
 
-    logic [13:0] Z1;
-    logic [13:0] CROSS;
+logic [11:0] Z0_w, Z2_w;
+logic [13:0] Z1_w; 
+logic [12:0]CROSS_w;
 
-    always_ff @(posedge clk or negedge rst_n) begin
+assign Z0_w    = A0 * B0;
+assign Z2_w    = A1 * B1;
+assign Z1_w    = (A0 + A1) * (B0 + B1);
+assign CROSS_w = Z1_w - Z2_w - Z0_w;
 
-        if (!rst_n) begin
+logic [11:0] Z0_r, Z2_r;
+logic [13:0] CROSS_r;
 
-            PRODUCT <= 24'd0;
+logic valid1;
 
-        end
-
-        else begin
-
-            A0 = A[5:0];
-            A1 = A[11:6];
-
-            B0 = B[5:0];
-            B1 = B[11:6];
-
-            Z0 = A0 * B0;
-
-            Z2 = A1 * B1;
-
-            SUM_A = A0 + A1;
-            SUM_B = B0 + B1;
-
-            Z1 = SUM_A * SUM_B;
-
-            CROSS = Z1 - Z2 - Z0;
-
-          PRODUCT = (Z2 << 12) + (CROSS << 6) + Z0;
-
-        end
-
+always_ff @(posedge clk or negedge rst_n)
+begin
+    if(!rst_n)
+    begin
+        Z0_r    <= 0;
+        Z2_r    <= 0;
+        CROSS_r <= 0;
+        valid1  <= 0;
     end
+    else
+    begin
+        Z0_r    <= Z0_w;
+        Z2_r    <= Z2_w;
+        CROSS_r <= CROSS_w;
+        valid1  <= 1'b1;
+    end
+end
 
+logic [23:0] PRODUCT;
+
+assign PRODUCT = (Z2_r << 12) +
+                 (CROSS_r << 6) +
+                  Z0_r;
+
+always_ff @(posedge clk or negedge rst_n)
+begin
+    if(!rst_n)
+    begin
+        h <= 0;
+        l  <= 0;
+    end
+    else if(valid1)
+    begin
+        h <= PRODUCT[23:12];
+        l  <= PRODUCT[11:0];
+    end
+end
+    
 endmodule
